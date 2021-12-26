a2d5d09271575ee80d4dfb5fc7d163211a6cee1d
Diego Anderson <dieanderson@gmail.com>
2021-12-26 12:25:34 -0300

    teste de link corrigido

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function Home(){
     return (
         <div>
             <h1>Home</h1>
-            <Link href="/sobre.js">
+            <Link href="/sobre">
                 <a>Acessar página Sobre</a>
             </Link>
         </div>
