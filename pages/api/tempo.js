d31899878294ad7b30053de148507d6420725624
Diego Anderson <dieanderson@gmail.com>
2021-12-26 23:38:56 -0300

    utiliza apis com variavel de ambiente

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,21 @@
-function tempo(request, response){
-    const dynamicDate = new Date();
+async function tempo(request, response){
+    const dynamicDate = new Date(); 
+
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
+    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
+    const subscriversResponseJson = await subscribersResponse.json();
+    const inscritos = subscriversResponseJson.total_subscribers;       
+
+    const triviaResponse = await fetch("https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple");
+    const triviaResponseJson = await triviaResponse.json();
+    const question = triviaResponseJson.results[0]['question'];
+    const correct_answer = triviaResponseJson.results[0]['correct_answer'];
 
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        inscritos: inscritos,
+        pergunta: question,
+        resposta: correct_answer
     })
 }
 
